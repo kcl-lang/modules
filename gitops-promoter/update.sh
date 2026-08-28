d1395502045c4a3caae2a056b0ca54ccc502ff73
daudcanugerah <34634382+daudcanugerah@users.noreply.github.com>
2026-08-28 17:51:35 +0700

    update git promoter to 0.36.0 (#394)

--- gitops-promoter/update.sh
+++ gitops-promoter/update.sh
@@ -4,13 +4,13 @@ set -o errexit
 set -o nounset
 set -eoux pipefail
 
-VERSION="v0.31.1"
+VERSION="v0.36.0"
 
 rm -rf v*
 mkdir -p crds
 rm -rf crds/*
 
-wget -O manifest.yaml "https://github.com/argoproj-labs/gitops-promoter/releases/download/${VERSION}/install.yaml"
+wget -O manifest.yaml "https://github.com/argoproj-labs/gitops-promoter/releases/download/${VERSION}/install-without-ui.yaml"
 
 yq eval 'select(.kind == "CustomResourceDefinition")' manifest.yaml >crds.yaml
 kcl import -m crd -o . ./crds.yaml
